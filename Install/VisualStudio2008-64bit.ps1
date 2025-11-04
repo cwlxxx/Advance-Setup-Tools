f81f1fad597cf5123e4a28d6987ea4049d1fe37a
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-11-04 20:11:27 +0800

    Create VisualStudio2008-64bit.ps1

---
+++ Install/VisualStudio2008-64bit.ps1
@@ -0,0 +1,11 @@
+# Visual C++ Redistributable (2008) - 64-bit
+
+Write-Host "🔍 Checking for Visual C++ 2008 (x64)..."
+
+if (-not (winget list --id Microsoft.VCRedist.2008.x64 | Select-String "Installed")) {
+    Write-Host "📦 Installing Visual C++ 2008 (x64)..." -ForegroundColor Cyan
+    winget install --id Microsoft.VCRedist.2008.x64 --source winget --exact --accept-package-agreements --accept-source-agreements
+    Write-Host "✅ Installation completed." -ForegroundColor Green
+} else {
+    Write-Host "✅ Visual C++ 2008 (x64) is already installed. Skipping." -ForegroundColor Yellow
+}
