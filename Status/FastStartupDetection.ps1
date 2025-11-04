bc72da498713656ef25a150bd845bb9ac1eeff16
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-11-04 19:58:15 +0800

    Create FastStartupDetection.ps1

---
+++ Status/FastStartupDetection.ps1
@@ -0,0 +1,17 @@
+
+function Get-FastStartupStatus {
+    try {
+        $key = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
+        $value = (Get-ItemProperty -Path $key -Name HiberbootEnabled -ErrorAction SilentlyContinue).HiberbootEnabled
+
+        switch ($value) {
+            1 { return "Enabled" }
+            0 { return "Disabled" }
+            default { return "Unknown" }
+        }
+    }
+    catch {
+        return "Error"
+    }
+}
+$faststartupstatus = Get-FastStartupStatus
