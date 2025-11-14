0f58e62048459223160746bc6d4985319b0b45b1
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-11-14 10:23:20 +0800

    Update VisualStudio2010-64bit.ps1

--- Install/VisualStudio2010-64bit.ps1
+++ Install/VisualStudio2010-64bit.ps1
@@ -1,11 +1,1 @@
-# Visual C++ Redistributable (2010) - 64-bit
-
-Write-Host "🔍 Checking for Visual C++ 2010 (x64)..."
-
-if (-not (winget list --id Microsoft.VCRedist.2010.x64 | Select-String "Installed")) {
-    Write-Host "📦 Installing Visual C++ 2010 (x64)..." -ForegroundColor Cyan
-    winget install --id Microsoft.VCRedist.2010.x64 --source winget --exact --accept-package-agreements --accept-source-agreements
-    Write-Host "✅ Installation completed." -ForegroundColor Green
-} else {
-    Write-Host "✅ Visual C++ 2010 (x64) is already installed. Skipping." -ForegroundColor Yellow
-}
+winget install --id Microsoft.VCRedist.2010.x64 --source winget --exact
