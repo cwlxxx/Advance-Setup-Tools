80417ee72c34e9bce998a62ce57c1797791baaef
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 14:58:07 +0800

    Update CPU-Z.ps1

--- Install/CPU-Z.ps1
+++ Install/CPU-Z.ps1
@@ -1,1 +1,2 @@
-winget install --id CPUID.CPU-Z --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id CPUID.CPU-Z --source winget --exact --ignore-security-hash
