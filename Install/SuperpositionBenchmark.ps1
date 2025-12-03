346582b65482732f85caec82806e9ab787fb05ce
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:15:28 +0800

    Update SuperpositionBenchmark.ps1

--- Install/SuperpositionBenchmark.ps1
+++ Install/SuperpositionBenchmark.ps1
@@ -1,1 +1,2 @@
-winget install --id Unigine.SuperpositionBenchmark --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id Unigine.SuperpositionBenchmark --source winget --exact --ignore-security-hash
