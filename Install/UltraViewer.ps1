5497e8fcad373c279a4d9ee3ea3d2e4b80097ca3
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:16:12 +0800

    Update UltraViewer.ps1

--- Install/UltraViewer.ps1
+++ Install/UltraViewer.ps1
@@ -1,1 +1,2 @@
-winget install --id DucFabulous.UltraViewer --source winget  --exact
+winget settings --enable InstallerHashOverride
+winget install --id DucFabulous.UltraViewer --source winget  --exact --ignore-security-hash
