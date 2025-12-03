327152dc7be0585c9b21a05b725643527f286984
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 14:58:23 +0800

    Update Chromium.ps1

--- Install/Chromium.ps1
+++ Install/Chromium.ps1
@@ -1,1 +1,2 @@
-winget install --id Hibbiki.Chromium --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id Hibbiki.Chromium --source winget --exact --ignore-security-hash
