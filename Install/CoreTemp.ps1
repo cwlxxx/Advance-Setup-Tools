45cafabc550fb133b4c5f5627222d45d43cec771
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 14:58:38 +0800

    Update CoreTemp.ps1

--- Install/CoreTemp.ps1
+++ Install/CoreTemp.ps1
@@ -1,1 +1,2 @@
-winget install --id ALCPU.CoreTemp --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id ALCPU.CoreTemp --source winget --exact --ignore-security-hash
