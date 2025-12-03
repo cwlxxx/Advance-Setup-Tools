d074059b46afab59ba5166f307aac3ce2f74c68e
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:23:41 +0800

    Update IrfanView.ps1

--- Install/IrfanView.ps1
+++ Install/IrfanView.ps1
@@ -1,6 +1,5 @@
-Write-Host "Installing IrfanView 64..." -ForegroundColor Cyan
 winget settings --enable InstallerHashOverride
-# Run the Winget installation (silent)
+
 Start-Process "winget.exe" -ArgumentList "install --id=IrfanSkiljan.IrfanView --source winget --exact --accept-package-agreements --accept-source-agreements --ignore-security-hash" -NoNewWindow -Wait
 
 Write-Host "⏳ Waiting for IrfanView installation to complete..." -ForegroundColor Cyan
