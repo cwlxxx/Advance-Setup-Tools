defda39afd69205daf6ee0db3915068911ff5e8c
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:01:36 +0800

    Update CrystalDiskMark.ps1

--- Install/CrystalDiskMark.ps1
+++ Install/CrystalDiskMark.ps1
@@ -1,1 +1,2 @@
-winget install --id CrystalDewWorld.CrystalDiskMark --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id CrystalDewWorld.CrystalDiskMark --source winget --exact --ignore-security-hash
