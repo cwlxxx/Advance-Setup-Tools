1228dd0c92851ec058922677105d6e8c5da349d2
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:07:26 +0800

    Update HeavenBenchmark.ps1

--- Install/HeavenBenchmark.ps1
+++ Install/HeavenBenchmark.ps1
@@ -1,1 +1,2 @@
-winget install --id Unigine.HeavenBenchmark --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id Unigine.HeavenBenchmark --source winget --exact --ignore-security-hash
