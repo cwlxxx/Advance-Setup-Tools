75aa1a73a9d43efc56eca9f5ac176afdb1be7c7e
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:04:25 +0800

    Update GPU-Z.ps1

--- Install/GPU-Z.ps1
+++ Install/GPU-Z.ps1
@@ -1,1 +1,2 @@
-winget install --id TechPowerUp.GPU-Z --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id TechPowerUp.GPU-Z --source winget --exact --ignore-security-hash
