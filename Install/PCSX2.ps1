9de26c615bfc89b9b6a23c00669ebe49a15068e1
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:13:42 +0800

    Update PCSX2.ps1

--- Install/PCSX2.ps1
+++ Install/PCSX2.ps1
@@ -1,1 +1,2 @@
-winget install --id PCSX2Team.PCSX2 --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id PCSX2Team.PCSX2 --source winget --exact --ignore-security-hash
