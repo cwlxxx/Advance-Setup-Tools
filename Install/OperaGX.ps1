8aed6b870a284ccccd8666a7de69ab04430b6f88
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:13:13 +0800

    Update OperaGX.ps1

--- Install/OperaGX.ps1
+++ Install/OperaGX.ps1
@@ -1,1 +1,2 @@
-winget install --id Opera.OperaGX --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id Opera.OperaGX --source winget --exact --ignore-security-hash
