a484d26d3f5d0d12deb542e283ea45daea6d148b
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:15:43 +0800

    Update Telegram.ps1

--- Install/Telegram.ps1
+++ Install/Telegram.ps1
@@ -1,1 +1,2 @@
-winget install --id Telegram.TelegramDesktop --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id Telegram.TelegramDesktop --source winget --exact --ignore-security-hash
