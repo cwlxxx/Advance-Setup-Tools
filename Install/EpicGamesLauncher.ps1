077ab1e1ce3620f52a033f5b2f6500bcc32652fa
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:03:25 +0800

    Update EpicGamesLauncher.ps1

--- Install/EpicGamesLauncher.ps1
+++ Install/EpicGamesLauncher.ps1
@@ -1,1 +1,2 @@
-winget install --id EpicGames.EpicGamesLauncher --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id EpicGames.EpicGamesLauncher --source winget --exact --ignore-security-hash
