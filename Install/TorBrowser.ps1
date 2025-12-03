d9b2ec1635dcf8cca7621292ca9399904c005fdf
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:15:56 +0800

    Update TorBrowser.ps1

--- Install/TorBrowser.ps1
+++ Install/TorBrowser.ps1
@@ -1,1 +1,2 @@
-winget install --id TorProject.TorBrowser --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id TorProject.TorBrowser --source winget --exact --ignore-security-hash
