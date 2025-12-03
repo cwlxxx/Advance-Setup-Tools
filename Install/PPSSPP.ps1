39fc4d67093c76369e33999c51695c5cd2c94d77
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-03 15:13:59 +0800

    Update PPSSPP.ps1

--- Install/PPSSPP.ps1
+++ Install/PPSSPP.ps1
@@ -1,1 +1,2 @@
-winget install --id PPSSPPTeam.PPSSPP --source winget --exact
+winget settings --enable InstallerHashOverride
+winget install --id PPSSPPTeam.PPSSPP --source winget --exact --ignore-security-hash
