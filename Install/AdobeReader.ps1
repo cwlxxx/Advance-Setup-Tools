2a34aa6d1b911573ce3d660bdb0150ad49b17600
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-23 21:10:24 +0800

    Update AdobeReader.ps1

--- Install/AdobeReader.ps1
+++ Install/AdobeReader.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id Adobe.Acrobat.Reader.64-bit --source winget --exact --ignore-security-hash
+winget install --id Adobe.Acrobat.Reader.64-bit --source winget --exact
