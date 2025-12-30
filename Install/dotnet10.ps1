8d88420ac6c214184041e8a8fa9a2bf960a702f7
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-30 12:14:03 +0800

    Update dotnet10.ps1

--- Install/dotnet10.ps1
+++ Install/dotnet10.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id Microsoft.DotNet.DesktopRuntime.10 --source winget --exact --ignore-security-hash
+winget install --id Microsoft.DotNet.DesktopRuntime.10 --source winget --exact
