94ee9917708edf34f7bd542686d6a64081fdb0ec
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-30 11:59:45 +0800

    Update OBSStudio.ps1

--- Install/OBSStudio.ps1
+++ Install/OBSStudio.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id OBSProject.OBSStudio --source winget --exact --ignore-security-hash
+winget install --id OBSProject.OBSStudio --source winget --exact
