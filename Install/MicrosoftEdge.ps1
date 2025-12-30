e96b9fe5da37242a5722a9369bec091b00194326
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-30 11:58:58 +0800

    Update MicrosoftEdge.ps1

--- Install/MicrosoftEdge.ps1
+++ Install/MicrosoftEdge.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id Microsoft.Edge --source winget --exact --ignore-security-hash
+winget install --id Microsoft.Edge --source winget --exact
