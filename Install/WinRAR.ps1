b1b940929092ba6e1c6938bd4f1043d8e4427fb7
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-30 12:11:08 +0800

    Update WinRAR.ps1

--- Install/WinRAR.ps1
+++ Install/WinRAR.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id RARLab.WinRAR --source winget --exact --ignore-security-hash
+winget install --id RARLab.WinRAR --source winget --exact
