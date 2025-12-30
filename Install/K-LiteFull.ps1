8a774275e2909a7dfbf041a4632ea249c49a4e4d
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-30 11:58:32 +0800

    Update K-LiteFull.ps1

--- Install/K-LiteFull.ps1
+++ Install/K-LiteFull.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id CodecGuide.K-LiteCodecPack.Full --source winget --exact --ignore-security-hash
+winget install --id CodecGuide.K-LiteCodecPack.Full --source winget --exact
