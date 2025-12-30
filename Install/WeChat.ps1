bdc81a6038c93c60911fc95f8092ba10d3531ea4
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-30 12:10:13 +0800

    Update WeChat.ps1

--- Install/WeChat.ps1
+++ Install/WeChat.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id Tencent.WeChat --source winget --exact --ignore-security-hash
+winget install --id Tencent.WeChat --source winget --exact
