f310090fcd94afa62296912fade075a790e55b09
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-30 11:15:25 +0800

    Update CrystalDiskInfo.ps1

--- Install/CrystalDiskInfo.ps1
+++ Install/CrystalDiskInfo.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id CrystalDewWorld.CrystalDiskInfo --source winget --exact --ignore-security-hash
+winget install --id CrystalDewWorld.CrystalDiskInfo --source winget --exact
