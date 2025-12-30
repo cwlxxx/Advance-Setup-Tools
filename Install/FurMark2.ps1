01feb270f4fcaa5ce247583ecb4627b3da6e2889
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-30 11:17:04 +0800

    Update FurMark2.ps1

--- Install/FurMark2.ps1
+++ Install/FurMark2.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id Geeks3D.FurMark.2 --source winget --exact --ignore-security-hash
+winget install --id Geeks3D.FurMark.2 --source winget --exact
