62d939c70f57a4481fa88c7ca3bdfd641831585f
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-30 11:16:52 +0800

    Update FireFox.ps1

--- Install/FireFox.ps1
+++ Install/FireFox.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id Mozilla.Firefox --source winget --exact --ignore-security-hash
+winget install --id Mozilla.Firefox --source winget --exact
