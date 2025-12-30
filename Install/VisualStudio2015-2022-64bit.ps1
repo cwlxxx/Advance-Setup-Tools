7de7d0111d90d1551f1bea5bcd529f3f435f8117
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-30 12:10:03 +0800

    Update VisualStudio2015-2022-64bit.ps1

--- Install/VisualStudio2015-2022-64bit.ps1
+++ Install/VisualStudio2015-2022-64bit.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id Microsoft.VCRedist.2015+.x64 --source winget --exact --ignore-security-hash
+winget install --id Microsoft.VCRedist.2015+.x64 --source winget --exact
