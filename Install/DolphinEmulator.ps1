744a5cf4d5c98e89da833c65138da5bfd0cdea28
Liang <cr29fjccyr@privaterelay.appleid.com>
2025-12-30 11:15:56 +0800

    Update DolphinEmulator.ps1

--- Install/DolphinEmulator.ps1
+++ Install/DolphinEmulator.ps1
@@ -1,2 +1,1 @@
-winget settings --enable InstallerHashOverride
-winget install --id DolphinEmulator.Dolphin --source winget --exact --ignore-security-hash
+winget install --id DolphinEmulator.Dolphin --source winget --exact
